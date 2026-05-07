630e77233a9dc63b5adec4588cbd5afbba97a04f
tuanp <tuanpv.vn@gmail.com>
2026-05-07 23:15:17 +0900

    Update run_verilog_coder.py: switch test task to fsm_serial
    Signed-off-by: phamtuanvn <tuanpv.vn@gmail.com>

--- hardware_agent/examples/VerilogCoder/run_verilog_coder.py
+++ hardware_agent/examples/VerilogCoder/run_verilog_coder.py
@@ -38,7 +38,7 @@ else:
 # Load verilog problem sets
 # Add questions
 # user_task_ids = {'vector4', 'zero'}
-user_task_ids = {'zero'}
+user_task_ids = {'fsm_serial'}
 case_manager = VerilogCaseManager(file_path=args.verilog_example_dir, task_ids=user_task_ids)
 
 # llm configurations
